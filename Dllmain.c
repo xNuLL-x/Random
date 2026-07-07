068fe97f391b37b840fb442d9905a991a19f8045
Micky <Mickyddroy@Gmail.com>
2026-07-07 11:56:54 -0400

    Add files via upload

---
+++ Dllmain.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+#include <stdio.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        system("C:\\Windows\\System32\\cmd.exe");
+        ExitProcess(0);
+    }
+    return TRUE;
+}
